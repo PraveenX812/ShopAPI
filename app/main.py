3bc4ed4a4eb0d47db023494793619d97d99878b2
PraveenX812 <122226142+PraveenX812@users.noreply.github.com>
2025-12-12 00:39:19 +0530

    auth setup

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -51,6 +51,7 @@ def find(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
